92ea5ddd26cc3a35478ce3f649ca11b417ed6d26
TheBrellis <brianroyalellis@gmail.com>
2020-01-03 18:12:28 -0500

    set up controller directory, imported express and burger.js, exporting router. still need to fill out routings for the page

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+//Importing orm model
+const burger = require("../models/burger.js");
+//Page Routings ================================================= 
+
+
+
+//===================================================================
+
+module.exports = router;
