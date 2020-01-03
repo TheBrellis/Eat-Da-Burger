562352dbd624a280c2b927a7c53c429c82d1e6e2
TheBrellis <brianroyalellis@gmail.com>
2020-01-03 17:55:13 -0500

    finished orm method for selectAll

--- config/orm.js
+++ config/orm.js
@@ -1,8 +1,12 @@
 const connection = require("../config/connection.js");
 
 const orm = {
-    selectAll: function(){
-
+    selectAll: function(cb){
+        let query = "SELECT * FROM burgers"
+        connection.query(query,function(err,res){
+            if (err) throw err;
+            cb(res);
+        })
     },
     insertOne: function(){
 
