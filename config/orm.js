637c61a61c158796241bce3992f5221600f3c683
TheBrellis <brianroyalellis@gmail.com>
2020-01-03 18:02:07 -0500

    finished setting up orm method updateOne

--- config/orm.js
+++ config/orm.js
@@ -2,23 +2,26 @@ const connection = require("../config/connection.js");
 
 const orm = {
     selectAll: function(cb){
-        let query = "SELECT * FROM burgers"
+        let query = "SELECT * FROM burgers;"
         connection.query(query,function(err,res){
             if (err) throw err;
             cb(res);
         })
     },
     insertOne: function(cols,vals,cb){
-        let query = `INSERT INTO burgers (${cols}) VALUES (${vals}) `;
-        
+        let query = `INSERT INTO burgers (${cols}) VALUES (${vals});`;
         connection.query(query, vals, function(err, res) {
             if (err) throw err;
             cb(res)
         })
 
     },
-    updateOne: function(){
-
+    updateOne: function(colVals, condition, cb){
+        let query = `Update burgers SET ${colVals} WHERE ${condition};`;
+        connection.query(query, function(err, res) {
+            if (err) throw err;
+            cb(res);
+        })
     }
 }
 module.exports = orm;
