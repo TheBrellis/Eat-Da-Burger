5fcc6b89c82daa68c66034f2dae8682761fff00d
TheBrellis <brianroyalellis@gmail.com>
2020-01-03 17:49:00 -0500

    Setting up application: initalized package.json and installed mysql, express, and express-handlebars packages set up db directory with schema and seeds set up config directory, completed connection.js, set up shells for orm queries

---
+++ config/orm.js
@@ -0,0 +1,14 @@
+const connection = require("../config/connection.js");
+
+const orm = {
+    selectAll: function(){
+
+    },
+    insertOne: function(){
+
+    },
+    updateOne: function(){
+
+    }
+}
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES
+("Dirty Burger",0),
+("Farmer Burger",0),
+("Everybodys Got One Vegan Burger",0)
+;
