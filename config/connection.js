985dfe2b921db3eb17cdd7abe849a33916e38936
TheBrellis <brianroyalellis@gmail.com>
2020-01-27 17:58:44 -0500

    furthur updates to allow for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
-
+var connection;
 if(process.env.JAWSDB_URL) {
-   const connection = mysql.createConnection(process.env.JAWSDB_URL);
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  const  connection = mysql.createConnection({
+    connection = mysql.createConnection({
         host: "localhost",
         user: "root",
         password: "Briguy_131",
